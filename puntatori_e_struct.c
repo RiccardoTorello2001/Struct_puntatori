d96262e65ffdc5dd6b0ad079a5f3d3ee720d4f7e
RiccardoTorello2001 <torello.riccardo01@gmail.com>
2021-12-02 12:44:55 +0100

    correzione del programma

--- puntatori_e_struct.c
+++ puntatori_e_struct.c
@@ -3,7 +3,7 @@
 struct Books{
   char title[64];
   int duration;
-}
+};
 
 void cambia_id(struct Books*);
 
@@ -11,10 +11,11 @@ int main() {
   struct Books pippo;
   struct Books* p_pippo = &pippo;
   pippo.duration=4;
-  cambia_id(p_pippo);
+ cambia_id(p_pippo);
   printf("Durata di pippo %d minuti\n",pippo.duration);
 
 }
+
 void cambia_id(struct Books* pluto)
 {
   pluto-> duration =5;
